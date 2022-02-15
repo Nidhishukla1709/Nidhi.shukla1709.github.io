e36732cf7e232ab0798108de5ff88f7037a378fd
Nidhi Shukla <43289190+Nidhishukla1709@users.noreply.github.com>
2022-02-15 14:48:22 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {UsersDataService} from './services/users-data.service'
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Service in Angular';
+  users:any;
+  constructor (private userData:UsersDataService)
+  {
+    userData.users().subscribe((data)=>{
+console.warn("data",data);
+      this.users=data});
+    
+  }
+  }
+
+
